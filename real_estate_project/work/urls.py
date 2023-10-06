eff4f636e8dc25fa696318dbe33dc271cb3bf11a
Ahsan Tariq <ahsantariq0724@gmail.com>
2023-10-06 21:25:00 +0500

    login and registration

--- real_estate_project/work/urls.py
+++ real_estate_project/work/urls.py
@@ -1,1 +1,53 @@
-urlpatterns = []
+from django.contrib.auth import views as auth_views
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path(
+        "login/",
+        auth_views.LoginView.as_view(
+            redirect_authenticated_user=True,
+            template_name="registration/login.html",
+            success_url="home",
+        ),
+        name="login",
+    ),
+    path(
+        "signup/",
+        views.SignUpView.as_view(template_name="registration/signup.html"),
+        name="signup",
+    ),
+    # Forget Password
+    path(
+        "password-reset/",
+        auth_views.PasswordResetView.as_view(
+            template_name="password-reset/password_reset.html",
+            subject_template_name="password-reset/password_reset_subject.txt",
+            email_template_name="password-reset/password_reset_email.html",
+            success_url="/login/",
+        ),
+        name="password_reset",
+    ),
+    path(
+        "password-reset/done/",
+        auth_views.PasswordResetDoneView.as_view(
+            template_name="password-reset/password_reset_done.html"
+        ),
+        name="password_reset_done",
+    ),
+    path(
+        "password-reset-confirm/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name="password-reset/password_reset_confirm.html"
+        ),
+        name="password_reset_confirm",
+    ),
+    path(
+        "password-reset-complete/",
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name="password-reset/password_reset_complete.html"
+        ),
+        name="password_reset_complete",
+    ),
+]
--- real_estate_project/work/views.py
+++ real_estate_project/work/views.py
@@ -1,1 +1,11 @@
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+
+from .forms import SignUpForm
+
 # Create your views here.
+
+
+class SignUpView(CreateView):
+    form_class = SignUpForm
+    success_url = reverse_lazy("home")
